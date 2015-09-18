97ceef89b3351fcc0b63d36fd523d66817ef5bc7
bryantj10 <bryantj10@nku.edu>
2015-09-18 00:34:42 -0400

    working on testCacheToFile.java

--- LRUProxy/LRUProxy/src/test/testCacheToFile.java
+++ LRUProxy/LRUProxy/src/test/testCacheToFile.java
@@ -1,2 +1,33 @@
+import java.io.*;
+
 public testCacheToFile{
+	
+	@Test
+	public void testRemove(){
+		
+	}
+	
+	@Test
+	public void testWrite(){
+		
+	}
+	
+	@Test
+	public void testRead(){
+		
+	}
+	
+	@Test
+	public void testIsCached(){
+		
+	}
+	
+	public static void main(String[] args){
+		List<String> sites = new ArrayList<String>();
+		Scanner fileScanner = new Scanner(new File("input.txt"));
+		while(fileScanner.)
+		CacheToFile cache = new CacheToFile();
+		
+		
+	}
 }
